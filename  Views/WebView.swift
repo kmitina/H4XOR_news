650f3231ffae5e4aedbd195d755326b3b44ef7fb
kris <kristmitina@gmail.com>
2020-12-07 15:04:29 +0100

    fixed bugs

---
+++  Views/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  DetailView.swift
+//  H4X0R_News
+//
+//  Created by DVKSH on 7.12.20.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+}
